59de0b8889dc1b2cf00bf5de4c262179c5402d57
avgladkovs <avgladkovs@gmail.com>
2019-05-07 00:03:05 +0300

    added bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,11 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
+//= require jquery
+//= require bootstrap-sprockets
 //= require_tree .
